e21f7180301c237f99e3214f245196690acdb838
Matthew Dunn <mjdunn7@yahoo.com>
2017-02-13 12:34:09 -0700

    Finished Product

---
+++ CS356_PA1/StreamCipher.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matthew on 2/12/17.
+//
+
+#include "streamCipher.h"
---
+++ CS356_PA1/StreamCipher.h
@@ -0,0 +1,14 @@
+//
+// Created by matthew on 2/12/17.
+//
+
+#ifndef CS356_PA1_STREAMCIPHER_H
+#define CS356_PA1_STREAMCIPHER_H
+
+
+class streamCipher {
+
+};
+
+
+#endif //CS356_PA1_STREAMCIPHER_H
